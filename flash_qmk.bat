ddecdbe7766741002fcb4ffbc1f873091e9401b1
SecretPocketCat <jiridexterhanc@gmail.com>
2021-12-18 21:03:34 +0100

    Updated flash bat scripts

---
+++ flash_qmk.bat
@@ -0,0 +1,5 @@
+@ECHO OFF
+
+.\target\release\annepro2_tools.exe --boot ..\annepro-qmk\annepro2_c18_main.bin
+
+pause
---
+++ flash_shine.bat
@@ -0,0 +1,5 @@
+@ECHO OFF
+
+.\target\release\annepro2_tools.exe --boot -t led ..\annepro2-shine\build\annepro2-shine-C18.bin
+
+pause
